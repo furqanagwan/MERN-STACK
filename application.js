a8985fb8e73e98115da717cf6bc3d55b4228580d
Furqan Agwan <furqankadri@gmail.com>
2019-11-15 14:59:16 +0000

    - Added JSON GET response example. - Added a new endpoint as an exmaple for a GET request. - Created production use JSON data which is not complete.

--- application.js
+++ application.js
@@ -1,5 +1,9 @@
+//define imports and import objects
 const express = require('express');
 const application = express();
+
+//Define local JSON data here
+var OctobersVeryOwn = require('./data/json/Artists.json');
 
 //Define your port number here, check with your server provider and thier configuration.
 
@@ -8,15 +12,32 @@ let defaultPORT = 3000;
 
 //Define your endpoints here of type String
 const root = "/";
+const artists = "/artists";
+
+
 
 /*
 Define all GET request for all endpoints here make all reponse use JSON as standard.
 */
 
-//Root GET request
+// //Root GET request response in String
+// application.get(root, (request, response) =>{
+//   console.log("root get request");
+//   response.send("Hello user, sucess")
+// });
+
+//Root GET request response in JSON
 application.get(root, (request, response) =>{
-  console.log("root get request");
-  response.send("Hello user, sucess")
+  response.json({
+    message: "Hello user, sucess"
+  })
+});
+
+//artists GET request response in JSON
+application.get(artists, (request, response) =>{
+  response.json({
+    OctobersVeryOwn
+  })
 });
 
 
