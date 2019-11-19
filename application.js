8e3985240d5b2f43dd816213433eadd053c5f906
Furqan Agwan <furqankadri@gmail.com>
2019-11-19 18:03:28 +0000

    - Started defining middleware rules - Cleaned up comments and removed useless code - Update JSON data - Added a test html page

--- application.js
+++ application.js
@@ -1,48 +1,31 @@
-//define imports and import objects
+//Define imports
 const express = require('express');
+//Create object of imports
 const application = express();
-
 //Define local JSON data here
 var OctobersVeryOwn = require('./data/json/Artists.json');
+//Define your port here
+let PORT = process.env.PORT || 3000;
 
-//Define your port number here, check with your server provider and thier configuration.
-
-//Use this PORT for testing purposes else use 8080
-let defaultPORT = 3000;
-
-//Define your endpoints here of type String
+//Define your endpoints here
 const root = "/";
 const artists = "/artists";
 
+//Define middleware here
 
+//Re route any incoming GET request to root to show a static webpage
+application.use(root, express.static(__dirname + '/data/html/testHTML.html'))
 
-/*
-Define all GET request for all endpoints here make all reponse use JSON as standard.
-*/
-
-// //Root GET request response in String
-// application.get(root, (request, response) =>{
-//   console.log("root get request");
-//   response.send("Hello user, sucess")
-// });
-
-//Root GET request response in JSON
+//GET Request for local html page on root directory
 application.get(root, (request, response) =>{
-  response.json({
-    message: "Hello user, sucess"
-  })
+  response.sendFile(__dirname + '/data/html/testHTML.html');
 });
 
-//artists GET request response in JSON
+// GET Request for local JSON data
 application.get(artists, (request, response) =>{
   response.json({
     OctobersVeryOwn
   })
 });
-
-
-application.listen(defaultPORT, () => {
-
-  console.log("Hello User, server is up");
-
-});
+// Tell the server to listen on the given port
+application.listen(PORT, () => {});
