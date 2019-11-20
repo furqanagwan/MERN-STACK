7580bea4136e2f881e89f31c0678a6baf49e94b6
Furqan Agwan <furqankadri@gmail.com>
2019-11-20 16:25:38 +0000

    - Added objects an cleaned up up some code - removed a single npm package

--- application.js
+++ application.js
@@ -1,7 +1,19 @@
+"use strict";
 //Define imports
 const express = require('express');
 //Create object of imports
 const application = express();
+
+let StatusCodes = {
+  Information: 100,
+  Sucess: 200,
+  Redirection: 300,
+  Client_error: 400,
+  Server_error: 500
+}
+
+let RequestType = {GET: "GET", POST: "POST"}
+
 //Define local JSON data here
 var OctobersVeryOwn = require('./data/json/Artists.json');
 //Define your port here
@@ -12,14 +24,23 @@ const root = "/";
 const artists = "/artists";
 
 //Define middleware here
-
 //Re route any incoming GET request to root to show a static webpage
 application.use(root, express.static(__dirname + '/data/html/testHTML.html'))
 
-//GET Request for local html page on root directory
-application.get(root, (request, response) =>{
-  response.sendFile(__dirname + '/data/html/testHTML.html');
+//express all fucntion for all types of request types
+application.all(root, (request, response) => {
+  if (response.statusCode == StatusCodes.Sucess){
+    response.sendFile(__dirname + '/data/html/testHTML.html');
+  }
 });
+
+
+// //GET Request for local html page on root directory
+// application.get(root, (request, response) =>{
+//   if (response.statusCode == StatusCodes.Sucess){
+//     response.sendFile(__dirname + '/data/html/testHTML.html');
+//   }
+// });
 
 // GET Request for local JSON data
 application.get(artists, (request, response) =>{
