2011af4eda5a087055d56c28b940f508747f77d1
Furqan Agwan <furqankadri@hotmail.com>
2020-04-16 00:10:51 +0100

    - UPDATED README.md

---
+++ client/src/Application.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './img/logo.svg';
+import './css/App.css';
+
+
+// Root webpage should house other webpages and other react componnets
+function Application() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Application;
--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const application = express();
 
 //Define port to which the express app will run on here, default will be 3001 because front end is on 3002
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 // Define what the express instance should use here
 application.set('view engine', 'ejs');
