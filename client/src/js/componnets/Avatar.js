cdc9549489dcf165cea2aaf5f5127b9b80b5889a
Furqan Agwan <furqankadri@gmail.com>
2020-08-02 16:09:15 +0100

    - changes to image size on react component

--- client/src/js/componnets/Avatar.js
+++ client/src/js/componnets/Avatar.js
@@ -5,7 +5,7 @@ export default class Avatar extends React.Component {
   render() {
     return (
       <div className = "Avatar-Container">
-       <img src= {AvatarImage} alt= "Furqan Agwan" height= "100" width= "100" />
+       <img src= {AvatarImage} alt= "Furqan Agwan" height= "200" width= "200" />
       </div>
     );
   }
